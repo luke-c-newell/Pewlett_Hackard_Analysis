bf41ada2af6a1041986c541f8bf4e33ab4dd1fc6
Luke Newell <lukenewell@Lukes-MacBook-Pro.local>
2020-11-10 13:36:36 -0600

    Adding Schema

--- schema.sql
+++ schema.sql
@@ -32,12 +32,12 @@ CREATE TABLE salaries (
   	PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_emp (
-	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
-  	from_date DATE NOT NULL,
-  	to_date DATE NOT NULL,
- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
@@ -48,4 +48,4 @@ CREATE TABLE titles (
  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
